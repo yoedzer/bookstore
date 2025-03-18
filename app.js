576a33cf894730d94a36645845761a99a39d3bd8
gwangchuk.sherubtse@rub.edu.bt <ugyeltongchuu98@gmail.com>
2025-03-18 18:17:03 +0600

    Practical 2

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const path = require('path');
 const dotenv  =  require('dotenv')
+const session = require('express-session');
+const db = require('./config/db'); 
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
 
 //load environment variables
 dotenv.config();
@@ -9,6 +13,15 @@ dotenv.config();
 // Initialize Express app
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+    secret: 'gyeltshenMk',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
 
 // Set view engine to EJS
 app.set('view engine', 'ejs');
@@ -20,6 +33,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Import routes
 const indexRoutes = require('./routes/index');
 app.use('/', indexRoutes);
+app.use('/auth', authRoutes);
+
+//for db connection
+app.get('/db-test', async (req, res) => {
+    try {
+        const result = await db.one('SELECT NOW() AS current_time');
+        res.json({ message: 'Database connected successfully', time: result.current_time });
+    } catch (err) {
+        res.status(500).json({ error: 'Database connection failed', details: err.message });
+    }
+});
 
 // Start server
 app.listen(PORT, () => {
---
+++ routes/authRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController');
+
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+router.post('/signup', authController.registerUser);
+router.post('/login', authController.loginUser);
+router.get('/logout', authController.logoutUser);
+
+module.exports = router;
