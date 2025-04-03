38e2292b1c7724561a62f62796bcbdfe21586432
yoedzer <drimey02@gmail.com>
2025-04-03 09:45:26 -0700

    Practical 4

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -5,6 +5,9 @@ const { isAuthenticated } = require('../config/authMiddleware');
 
 // Profile route (only accessible by logged-in users)
 router.get('/profile', isAuthenticated, (req, res) => {
+    if (!req.session.user) {
+        return res.redirect("/auth/login"); // Redirect if not logged in
+    }
     res.render('profile', { user: req.session.user });
 });
 
